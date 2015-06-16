9c423c9c26855a08c644bcfce9f85dd56478a2ec
Dalton Boll <daltonboll@gmail.com>
2015-06-16 10:57:18 -0700

    Annotate Comment model file

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  email       :string
+#  text        :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  tutorial_id :integer
+#
+
 class Comment < ActiveRecord::Base
   belongs_to :tutorial
 end
