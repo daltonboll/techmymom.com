a310a5b73cde3c277a6666b277dd11f48fb5156c
Dalton Boll <daltonboll@gmail.com>
2015-06-16 10:47:36 -0700

    Annotate Video model file

--- test/models/video_test.rb
+++ test/models/video_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: videos
+#
+#  id          :integer          not null, primary key
+#  url         :text
+#  title       :string
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  tutorial_id :integer
+#
+
 require 'test_helper'
 
 class VideoTest < ActiveSupport::TestCase
